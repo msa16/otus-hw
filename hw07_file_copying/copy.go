f6184115dea8c3c53fbee27bc421542f798dacc8
Sergey Mironovich <mironovichs@mail.ru>
2024-10-27 22:51:25 +0300

    HW IO completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,130 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
+
+const COPY_STEP = 8192
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrInvalidOffset         = errors.New("offset cannot be negative")
+	ErrInvalidLimit          = errors.New("limit cannot be negative")
+	ErrInvalidLimitPositive  = errors.New("limit must be positive")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+type CopyInfo struct {
+	fromPath    string
+	toPath      string
+	offset      int64
+	limit       int64
+	bytesToCopy int64
+	fromFile    *os.File
+	toFile      *os.File
+}
+
+func NewCopyInfo(fromPath, toPath string, offset, limit int64) *CopyInfo {
+	return &CopyInfo{
+		fromPath: fromPath,
+		toPath:   toPath,
+		offset:   offset,
+		limit:    limit,
+	}
+}
+
+func (ci *CopyInfo) openSrcFile() error {
+	if ci.offset < 0 {
+		return ErrInvalidOffset
+	}
+	if ci.limit < 0 {
+		return ErrInvalidLimit
+	}
+	var err error
+	ci.fromFile, err = os.Open(ci.fromPath)
+	if err != nil {
+		return err
+	}
+	// проверки для входного файла
+	fromFileInfo, err := ci.fromFile.Stat()
+	if err != nil {
+		return err
+	}
+	if fromFileInfo.IsDir() {
+		return ErrUnsupportedFile
+	}
+	if fromFileInfo.Mode().IsRegular() {
+		// offset больше, чем размер файла - невалидная ситуация;
+		if ci.offset > fromFileInfo.Size() {
+			return ErrOffsetExceedsFileSize
+		}
+		ci.bytesToCopy = fromFileInfo.Size() - ci.offset
+		if ci.limit > 0 {
+			ci.bytesToCopy = min(ci.limit, ci.bytesToCopy)
+		}
+	} else {
+		// размер файла неизвестен, тогда нужен лимит
+		if ci.limit == 0 {
+			return ErrInvalidLimitPositive
+		}
+		ci.bytesToCopy = ci.limit
+	}
+	if ci.offset > 0 {
+		// проматываем от начала входного файла
+		_, err = ci.fromFile.Seek(ci.offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func (ci *CopyInfo) doCopy() error {
+	// start new bar
+	bar := pb.Full.Start64(ci.bytesToCopy)
+	defer bar.Finish()
+	// create proxy reader
+	barReader := bar.NewProxyReader(ci.fromFile)
+
+	for bytesCopied := int64(0); bytesCopied < ci.bytesToCopy; {
+		stepToCopy := min(COPY_STEP, ci.bytesToCopy-bytesCopied)
+		written, err := io.CopyN(ci.toFile, barReader, stepToCopy)
+		bytesCopied += written
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+	}
+	return nil
+}
+
+func (ci *CopyInfo) Copy() error {
+	// открываем входной файл
+	err := ci.openSrcFile()
+	if err != nil {
+		if ci.fromFile != nil {
+			ci.fromFile.Close()
+		}
+		return err
+	}
+	defer ci.fromFile.Close()
+
+	// создаем выходной файл
+	ci.toFile, err = os.Create(ci.toPath)
+	if err != nil {
+		return err
+	}
+	defer ci.toFile.Close()
+
+	return ci.doCopy()
+}
+
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	ci := NewCopyInfo(fromPath, toPath, offset, limit)
+	return ci.Copy()
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,74 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func testOneFile(t *testing.T, tmpFileName, expectedFileName string, offset, limit int64) {
+	err := Copy("testdata/input.txt", tmpFileName, offset, limit)
+	require.NoError(t, err)
+	actual, _ := os.ReadFile(tmpFileName)
+	expected, _ := os.ReadFile(expectedFileName)
+	require.Equal(t, bytes.Compare(actual, expected), 0)
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("error cases", func(t *testing.T) {
+		err := Copy("", "", 0, 0)
+		if _, ok := err.(*os.PathError); !ok {
+			require.Fail(t, "err is not *os.PathError")
+		}
+
+		err = Copy("/dev/random", "///", 1, 1)
+		if _, ok := err.(*os.PathError); !ok {
+			require.Fail(t, "err is not *os.PathError")
+		}
+
+		err = Copy("", "/dev/null", 0, 0)
+		if _, ok := err.(*os.PathError); !ok {
+			require.Fail(t, "err is not *os.PathError")
+		}
+
+		// не сможет записать
+		err = Copy("/dev/random", "/dev/full", 0, 1)
+		if _, ok := err.(*os.PathError); !ok {
+			require.Fail(t, "err is not *os.PathError")
+		}
+
+		err = Copy("", "", -1, 0)
+		require.ErrorIs(t, err, ErrInvalidOffset)
+
+		err = Copy("", "", 0, -1)
+		require.ErrorIs(t, err, ErrInvalidLimit)
+
+		err = Copy(".", "/dev/null", 0, 0)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+
+		err = Copy("/dev/random", "/dev/null", 0, 0)
+		require.ErrorIs(t, err, ErrInvalidLimitPositive)
+
+	})
+	t.Run("success cases", func(t *testing.T) {
+		err := Copy("/dev/random", "/dev/null", 0, 100)
+		require.NoError(t, err)
+
+		err = Copy("/dev/random", "/dev/null", 100, 100)
+		require.NoError(t, err)
+	})
+	t.Run("test file data", func(t *testing.T) {
+		f, _ := os.CreateTemp("", "copy_test")
+		defer os.Remove(f.Name())
+		f.Close()
+
+		testOneFile(t, f.Name(), "testdata/out_offset0_limit0.txt", 0, 0)
+		testOneFile(t, f.Name(), "testdata/out_offset0_limit10.txt", 0, 10)
+		testOneFile(t, f.Name(), "testdata/out_offset0_limit1000.txt", 0, 1000)
+		testOneFile(t, f.Name(), "testdata/out_offset0_limit10000.txt", 0, 10000)
+		testOneFile(t, f.Name(), "testdata/out_offset100_limit1000.txt", 100, 1000)
+		testOneFile(t, f.Name(), "testdata/out_offset6000_limit1000.txt", 6000, 1000)
+	})
 }
